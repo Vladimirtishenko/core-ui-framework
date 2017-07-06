b388b296ecf8e7cf3bf1f7e4fe88ed0984f08dea
vladimir <valdimirtishenko1@gmail.com>
2017-07-06 22:54:25 +0300

    add styles and smoth method

--- components/helper/transition.js
+++ components/helper/transition.js
@@ -93,6 +93,35 @@ class TransitionHelper {
         }, 10);
 	}
 
+	static doScrolling(element, duration) {
+		let startingY = window.pageYOffset,
+			elementY = window.pageYOffset + document.querySelector(element).getBoundingClientRect().top,
+			targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY,
+			diff = targetY - startingY,
+			easing = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+			start;
+
+	  	if (!diff) return
+		// Bootstrap our animation - it will get called right before next frame shall be rendered.
+		window.requestAnimationFrame(function step(timestamp) {
+	    if (!start) start = timestamp
+	    // Elapsed miliseconds since start of scrolling.
+	    var time = timestamp - start
+			// Get percent of completion in range [0, 1].
+	    var percent = Math.min(time / duration, 1)
+	    // Apply the easing.
+	    // It can cause bad-looking slow frames in browser performance tool, so be careful.
+	    percent = easing(percent)
+
+	    window.scrollTo(0, startingY + diff * percent)
+
+		// Proceed with animation as long as we wanted it to.
+	    if (time < duration) {
+	      window.requestAnimationFrame(step)
+	    }
+	  })
+	}
+
 }
 
 export default TransitionHelper;
