6982fe5aff35093d5e19dd9cc02eb97835b7df39
Владимир Тищенко <valdimirtishenko>
2018-09-11 14:00:01 +0300

    remove reset.css

--- components/modules/modal/modal.md.js
+++ components/modules/modal/modal.md.js
@@ -19,14 +19,14 @@ class ModalModule {
 	open(html, options){
 
 		this.props = {...ModalModule.defaultProps, ...options};
-	
+
 		let view = this.props.view ? this.view.template(html) : html;
 		document.body.insertAdjacentHTML('afterbegin', view);
 
 		this.wrapper = _$('.'+this.props.wrapper);
 
 		if(!this.wrapper) return;
-		
+
 		this.$public.helper('transition').openTransition(this.wrapper, 'flex', this.props.transition, 'opacity: 1');
 
 		this.close();
@@ -39,7 +39,7 @@ class ModalModule {
 			this.$public.helper('transition').closeTransition(this.wrapper, 'none', 'opacity: 0', this.props.remove);
 		})
 
-		
+
 	}
 }
 
